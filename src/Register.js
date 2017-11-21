c746ec312b20ed318a495f8d4a9381b1be142fc6
Charlon Palacay <charlon@gmail.com>
2017-11-20 20:34:54 -0800

    Fixes register page.

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Register = (props) => {
-    return <h1>Register</h1>
-}
+const Register = () => (
+    <div>
+        <h1>Register</h1>
+        <h2>Winter Quarter 2018</h2>
+        <p><a href="/courses">Find courses</a></p>
+    </div>
+)
 
 
 export default Register;
