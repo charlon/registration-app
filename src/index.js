0ffcefba8b91c5859f7e75a5b4538ade32777aaa
Charlon Palacay <charlon@gmail.com>
2018-09-18 17:06:55 -0700

    Updated npm audit.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+        <App/>
+    </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
